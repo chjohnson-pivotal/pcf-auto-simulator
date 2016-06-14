fbd948b214294fb5b9a4043ead7175a07e6cef96
chjohnson@pivotal.io <chjohnson@pivotal.io>
2016-06-14 14:40:32 -0400

    working zipkin server for local or PWS deployments

---
+++ zipkin-server/src/main/java/io/pivotal/demo/ZipkinStreamServerApplication.java
@@ -0,0 +1,15 @@
+package io.pivotal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+public class ZipkinStreamServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinStreamServerApplication.class, args);
+	}
+}
--- zipkin-server/src/test/java/io/pivotal/demo/ZipkinStreamServerApplicationTests.java
+++ zipkin-server/src/test/java/io/pivotal/demo/ZipkinStreamServerApplicationTests.java
@@ -1,4 +1,6 @@
-package com.example;
+package io.pivotal.demo;
+
+import io.pivotal.demo.ZipkinStreamServerApplication;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -6,8 +8,8 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-public class DemoApplicationTests {
+@SpringApplicationConfiguration(classes = ZipkinStreamServerApplication.class)
+public class ZipkinStreamServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
